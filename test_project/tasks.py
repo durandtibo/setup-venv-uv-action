8de2b9c4f5920a4ca92496071e7cb6a8569a20bf
Thibaut Durand <durand.tibo@gmail.com>
2025-12-11 08:18:45 -0800

    Create test project (#5)

---
+++ test_project/tasks.py
@@ -0,0 +1,29 @@
+r"""Define some tasks that are executed with invoke."""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from invoke.tasks import task
+
+if TYPE_CHECKING:
+    from invoke.context import Context
+
+@task
+def install(c: Context) -> None:
+    r"""Install packages."""
+    c.run("uv sync --frozen", pty=True)
+    c.run("uv pip install -e .", pty=True)
+    
+@task
+def show_installed_packages(c: Context) -> None:
+    r"""Show the installed packages."""
+    c.run("uv pip list", pty=True)
+
+@task
+def show_python_config(c: Context) -> None:
+    r"""Show the python configuration."""
+    c.run("uv python list --only-installed", pty=True)
+    c.run("uv python find", pty=True)
+    c.run("which python", pty=True)
+
